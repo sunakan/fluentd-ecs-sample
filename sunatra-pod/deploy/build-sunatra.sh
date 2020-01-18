9529e1100a355ed18ec0f7aa96bea2435741a99f
sunakan <sunakan03@gmail.com>
2020-01-18 15:39:52 +0900

    Modify to re-tag sunatra docker image tag

--- sunatra-pod/deploy/build-sunatra.sh
+++ sunatra-pod/deploy/build-sunatra.sh
@@ -4,7 +4,8 @@ set -u
 set -e
 
 docker build ../sunatra/ \
-  --tag ${DOCKER_SUNATRA_REPOSITORY}:${DOCKER_RUBY_TAG} \
+  --tag ${DOCKER_SUNATRA_REPOSITORY}:latest \
   --build-arg DOCKER_RUBY_TAG=${DOCKER_RUBY_TAG}
+docker tag ${DOCKER_SUNATRA_REPOSITORY}:latest ${DOCKER_SUNATRA_REPOSITORY}:${DOCKER_RUBY_TAG}
 
 echo "${DOCKER_SUNATRA_REPOSITORY}:${DOCKER_RUBY_TAG}"
